8bf22d72a3a4176d821727b63383da2eddcbdb7c
Jan Masny <j_masny@utb.cz>
2023-12-10 02:00:38 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ap5pmProjekt',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
